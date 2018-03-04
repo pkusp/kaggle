d627dfbbff1cb73b6cd2e4afb60b0c2ebc8f0b8c
pkusp <pkusp@outlook.com>
2018-03-04 23:50:01 +0800

    quora data

---
+++ quora/python_trick.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+
+
+
+
+In [6]: x=666
+In [7]: print('Total number of question pairs for training: {}'.format( x ))
+out[]: 'Total number of question pairs for training: 666'
+
